147a94f80569f8c7bce53aac7a4dee238ab8f0cd
Daiki Kohama <kohama@ucl.nuee.nagoya-u.ac.jp>
2024-01-29 16:59:49 +0900

    chage FAST to ORB

--- src/stella_vslam/feature/orb_extractor.cc
+++ src/stella_vslam/feature/orb_extractor.cc
@@ -190,8 +190,13 @@ void orb_extractor::compute_fast_keypoints(std::vector<std::vector<cv::KeyPoint>
                 }
 
                 std::vector<cv::KeyPoint> keypts_in_cell;
+                /*
                 cv::FAST(image_pyramid_.at(level).rowRange(min_y, max_y).colRange(min_x, max_x),
                          keypts_in_cell, orb_params_->ini_fast_thr_, true);
+                         */
+                cv::Ptr<cv::ORB> orb = cv::ORB::create(1000000);
+                orb->detectAndCompute(image_pyramid_.at(level).rowRange(min_y, max_y).colRange(min_x, max_x),
+                                    cv::Mat(), keypts_in_cell, cv::noArray());
 
                 // Re-compute FAST keypoint with reduced threshold if enough keypoint was not got
                 if (keypts_in_cell.empty()) {
